28ca0ede611c00c5bc7aab8691290caaa4545ba6
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-05-08 12:43:17 -0300

    feat: add middleware rabbitmq

---
+++ src/common/middleware/middleware_rabbitmq.py
@@ -0,0 +1,152 @@
+import pika
+import socket
+from .middleware import (
+    MessageMiddlewareQueue, 
+    MessageMiddlewareExchange, 
+    MessageMiddlewareCloseError, 
+    MessageMiddlewareMessageError,
+    MessageMiddlewareDisconnectedError
+)
+
+_CONNECTION_ERRORS = (
+    pika.exceptions.AMQPConnectionError,
+    pika.exceptions.AMQPChannelError,
+    pika.exceptions.StreamLostError
+)
+
+class _RabbitMQBase:
+    def __init__(self, host):
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self._channel = self._connection.channel()
+        self._user_callback = None
+        self._queue_name = None
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        return False
+
+    def _on_messaging_callback_adapter(self, ch, method, properties, body):
+        ack = lambda: ch.is_open and ch.basic_ack(delivery_tag=method.delivery_tag)
+        nack = lambda: ch.is_open and ch.basic_nack(delivery_tag=method.delivery_tag)
+        self._user_callback(body, ack, nack)
+
+    
+    def start_consuming(self, on_messaging_callback):
+        try:
+            self._user_callback = on_messaging_callback
+            self._channel.basic_qos(prefetch_count=1)
+            self._channel.basic_consume(
+                queue=self._queue_name, 
+                on_message_callback=self._on_messaging_callback_adapter)
+            self._channel.start_consuming()
+        except _CONNECTION_ERRORS as e:
+            raise MessageMiddlewareDisconnectedError(e)
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e)
+
+    def stop_consuming(self):
+        try:
+            if self._channel:
+                self._channel.stop_consuming()
+        except _CONNECTION_ERRORS as e:
+            raise MessageMiddlewareDisconnectedError(e)
+
+    def close(self):
+        errors = []
+        for resource in (self._channel, self._connection):
+            try:
+                if resource and resource.is_open:
+                    resource.close()
+            except Exception as e:
+                errors.append(e)
+
+        if errors:
+            raise MessageMiddlewareCloseError(errors[0])
+
+
+
+class MessageMiddlewareQueueRabbitMQ(_RabbitMQBase, MessageMiddlewareQueue):
+
+    def __init__(self, host, queue_name):
+        super().__init__(host)
+        self._queue_name = queue_name
+        self._channel.queue_declare(queue=queue_name, durable=True)
+
+    def send(self, message):
+        try:
+            self._channel.basic_publish(exchange="", routing_key=self._queue_name, body=message)
+        except _CONNECTION_ERRORS as e:
+            raise MessageMiddlewareDisconnectedError(e)
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e)
+
+
+
+        
+class MessageMiddlewareExchangeRabbitMQ(_RabbitMQBase, MessageMiddlewareExchange):
+    
+    def __init__(self, host, exchange_name, routing_keys, exchange_type="direct"):
+        super().__init__(host)
+        self._exchange_name = exchange_name
+        self._exchange_type = exchange_type
+        self._exchange = self._channel.exchange_declare(
+            exchange=exchange_name,
+            exchange_type=exchange_type,
+            durable=True,
+        )
+        self._routing_keys = routing_keys
+
+    
+    def send(self, message):
+        try:
+            if self._exchange_type == 'fanout':
+                self._channel.basic_publish(
+                    exchange=self._exchange_name,
+                    routing_key='',
+                    body=message,
+                )
+            else:
+                if not self._routing_keys:
+                    raise MessageMiddlewareMessageError("No routing keys provided")
+                
+                for key in self._routing_keys:
+                    self._channel.basic_publish(
+                        exchange=self._exchange_name,
+                        routing_key=key,
+                        body=message,
+                    )
+        except _CONNECTION_ERRORS as e:
+            raise MessageMiddlewareDisconnectedError(e)
+        except Exception as e:
+            raise MessageMiddlewareMessageError(e)
+
+
+    def start_consuming(self, on_messaging_callback):
+        self._init_queue()
+        super().start_consuming(on_messaging_callback)
+
+    def _init_queue(self):
+        if self._queue_name: 
+            return
+        queue_result = self._channel.queue_declare(queue='', exclusive=True)
+        self._queue_name = queue_result.method.queue
+
+        if self._exchange_type == 'fanout':
+            self._channel.queue_bind(
+                queue=self._queue_name,
+                exchange=self._exchange_name,
+                routing_key=''
+            )
+        else:
+            for key in self._routing_keys:
+                self._channel.queue_bind(
+                    queue=self._queue_name,
+                    exchange=self._exchange_name,
+                    routing_key=key
+                )
+
+
+    
