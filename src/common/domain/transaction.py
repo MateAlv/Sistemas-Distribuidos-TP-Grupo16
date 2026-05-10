9cbe496ab4eb64a00f45cc92536de7eb792988b7
santiagoFassio <fassiosantiago@gmail.com>
2026-05-10 19:21:47 -0300

    fix: transaction functions

--- src/common/domain/transaction.py
+++ src/common/domain/transaction.py
@@ -33,6 +33,12 @@ class Transaction:
     def is_in_date_range(self, start_date, end_date):
         return start_date <= self.date <= end_date
     
+    def get_payment_format(self):
+        return self.format
+    
+    def get_currency(self):
+        return self.currency
+    
     def hash_by_payment_format(self, num_buckets):
         h = 0
         for c in self.format:
