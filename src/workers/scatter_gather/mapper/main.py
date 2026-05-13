2ffe3c3fde02bed314ab6da0c6acf2210ae82f61
Mate <mateoalvarez2012@gmail.com>
2026-05-12 21:24:20 -0300

    mapper! first impl

--- src/workers/scatter_gather/mapper/main.py
+++ src/workers/scatter_gather/mapper/main.py
@@ -1,4 +1,17 @@
+import logging
+import signal
+
+from mapper import ScatterGatherMapper
+
+
 def main() -> int:
+    logging.basicConfig(level=logging.INFO)
+    worker = ScatterGatherMapper()
+    signal.signal(signal.SIGTERM, lambda *_: worker.handle_sigterm())
+    try:
+        worker.start()
+    finally:
+        worker.close()
     return 0
 
 
