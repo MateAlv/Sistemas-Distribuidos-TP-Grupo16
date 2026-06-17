24bbcf0cf68e6388fcc6c8ed3a20b2195de55c93
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-06-17 19:56:35 -0300

    feat: implement WALWriter for append-only record handling

--- src/common/fault_tolerance/wal/__init__.py
+++ src/common/fault_tolerance/wal/__init__.py
@@ -3,5 +3,6 @@ from common.fault_tolerance.wal.input_done import InputDone
 from common.fault_tolerance.wal.record import RecordType
 from common.fault_tolerance.wal.wal import Wal
 from common.fault_tolerance.wal.wal_record import WalRecord
+from common.fault_tolerance.wal.writer import WALWriter
 
-__all__ = ["InputApplied", "InputDone", "RecordType", "Wal", "WalRecord"]
+__all__ = ["InputApplied", "InputDone", "RecordType", "Wal", "WalRecord", "WALWriter"]
