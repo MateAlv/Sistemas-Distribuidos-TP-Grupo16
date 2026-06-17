ddd3a90d6a560695b0207c77351260d326d2aa7a
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-06-17 20:31:15 -0300

    feat: enhance WALReader with new record types and decoding functionality

--- src/common/fault_tolerance/wal/__init__.py
+++ src/common/fault_tolerance/wal/__init__.py
@@ -1,8 +1,41 @@
 from common.fault_tolerance.wal.input_applied import InputApplied
 from common.fault_tolerance.wal.input_done import InputDone
+from common.fault_tolerance.wal.reader import (
+    Checkpoint,
+    ClientCleanupStarted,
+    DecodedRecord,
+    EofSent,
+    WALRawRecord,
+    WALReader,
+    decode_checkpoint,
+    decode_client_cleanup_started,
+    decode_eof_sent,
+    decode_input_applied,
+    decode_input_done,
+    decode_record,
+)
 from common.fault_tolerance.wal.record import RecordType
 from common.fault_tolerance.wal.wal import Wal
 from common.fault_tolerance.wal.wal_record import WalRecord
 from common.fault_tolerance.wal.writer import WALWriter
 
-__all__ = ["InputApplied", "InputDone", "RecordType", "Wal", "WalRecord", "WALWriter"]
+__all__ = [
+    "Checkpoint",
+    "ClientCleanupStarted",
+    "DecodedRecord",
+    "EofSent",
+    "InputApplied",
+    "InputDone",
+    "RecordType",
+    "Wal",
+    "WalRecord",
+    "WALRawRecord",
+    "WALReader",
+    "WALWriter",
+    "decode_checkpoint",
+    "decode_client_cleanup_started",
+    "decode_eof_sent",
+    "decode_input_applied",
+    "decode_input_done",
+    "decode_record",
+]
